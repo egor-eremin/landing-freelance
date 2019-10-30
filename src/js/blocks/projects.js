03379f411d53794430e55b0052bf9b6cd6cf3aa2
Egor <egor_eremin@mail.ru>
2019-10-30 17:04:39 +0800

    new edits

--- src/js/blocks/projects.js
+++ src/js/blocks/projects.js
@@ -1,75 +1,75 @@
-(function initProjectSlider() {
-    classSlider = function (owl) {
-        var transitionTime = 6000;
-        var counter;
-        var timeInterval;
-        var timer = 0;
-
-        owl.viewportChecker({
-            callbackFunction: function (elem) {
-                setTimeout(function () {
-                    owl.find('.owl-dot:first-child').addClass('animate');
-                }, 10);
-                startAutoplay();
-            },
-        });
-        // owl.on('initialized.owl.carousel', function () {
-        //     setTimeout(function () {
-        //         owl.find('.owl-dot:first-child').addClass('animate');
-        //     }, 10);
-        //     startAutoplay();
-        // });
-        owl.owlCarousel({
-            items: 1,
-            smartSpeed: '300',
-            loop: true,
-            autoplayHoverPause: false,
-        });
-        owl.on('translated.owl.carousel', function(event) {
-            var thisSlider = $(this);
-
-            thisSlider.find('.owl-dot.active').prevAll().addClass('stopAnimation');
-            thisSlider.find('.owl-dot.active ~ .owl-dot').removeClass('animate');
-            thisSlider.find('.owl-dot.active ~ .owl-dot').removeClass('stopAnimation');
-            thisSlider.find('.owl-dot.active').removeClass('stopAnimation');
-            thisSlider.find('.owl-dot.active').removeClass('animate');
-            setTimeout(function () {
-                thisSlider.find('.owl-dot.active').addClass('animate');
-            }, 0.01);
-
-            timer = 0;
-        });
-
-        function initTimer() {
-            counter = setTimeout(function timeTick() {
-                timer += 10;
-                counter = setTimeout(timeTick, 10);
-            }, 10);
-        }
-        function startAutoplay() {
-            initTimer();
-            timeInterval = setInterval(function () {
-                if (timer >= transitionTime - 300) {
-                    goSlide();
-                    timer = 0;
-                }
-            }, 10);
-
-        };
-        function goSlide() {
-            owl.trigger('next.owl.carousel');
-        };
-        owl.hover(function () {
-            clearTimeout(counter);
-        }, function () {
-            initTimer();
-        });
-    };
-
-    $('.projects-slider').each(function (index, value) {
-        objSlider = new classSlider($(value));
-    })
-})();
+// (function initProjectSlider() {
+//     classSlider = function (owl) {
+//         var transitionTime = 6000;
+//         var counter;
+//         var timeInterval;
+//         var timer = 0;
+//
+//         owl.viewportChecker({
+//             callbackFunction: function (elem) {
+//                 setTimeout(function () {
+//                     owl.find('.owl-dot:first-child').addClass('animate');
+//                 }, 10);
+//                 startAutoplay();
+//             },
+//         });
+//         // owl.on('initialized.owl.carousel', function () {
+//         //     setTimeout(function () {
+//         //         owl.find('.owl-dot:first-child').addClass('animate');
+//         //     }, 10);
+//         //     startAutoplay();
+//         // });
+//         owl.owlCarousel({
+//             items: 1,
+//             smartSpeed: '300',
+//             loop: true,
+//             autoplayHoverPause: false,
+//         });
+//         owl.on('translated.owl.carousel', function(event) {
+//             var thisSlider = $(this);
+//
+//             thisSlider.find('.owl-dot.active').prevAll().addClass('stopAnimation');
+//             thisSlider.find('.owl-dot.active ~ .owl-dot').removeClass('animate');
+//             thisSlider.find('.owl-dot.active ~ .owl-dot').removeClass('stopAnimation');
+//             thisSlider.find('.owl-dot.active').removeClass('stopAnimation');
+//             thisSlider.find('.owl-dot.active').removeClass('animate');
+//             setTimeout(function () {
+//                 thisSlider.find('.owl-dot.active').addClass('animate');
+//             }, 0.01);
+//
+//             timer = 0;
+//         });
+//
+//         function initTimer() {
+//             counter = setTimeout(function timeTick() {
+//                 timer += 10;
+//                 counter = setTimeout(timeTick, 10);
+//             }, 10);
+//         }
+//         function startAutoplay() {
+//             initTimer();
+//             timeInterval = setInterval(function () {
+//                 if (timer >= transitionTime - 300) {
+//                     goSlide();
+//                     timer = 0;
+//                 }
+//             }, 10);
+//
+//         };
+//         function goSlide() {
+//             owl.trigger('next.owl.carousel');
+//         };
+//         owl.hover(function () {
+//             clearTimeout(counter);
+//         }, function () {
+//             initTimer();
+//         });
+//     };
+//
+//     $('.projects-slider').each(function (index, value) {
+//         objSlider = new classSlider($(value));
+//     })
+// })();
 (function showMoreProject() {
     var startHeight = 0;
 
